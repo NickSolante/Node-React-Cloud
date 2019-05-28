0a92b3106a17af0371761aa484d2f4092bba1e0d
frostnovus <psolante@deakin.edu.au>
2019-05-28 11:55:26 +1000

    something weird happening with database

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword =
+  "mongodb+srv://NickTheAdmin:9t7biu12T5hl1H3e@cluster0-zgphg.mongodb.net/test?retryWrites=true";
+
+module.exports = {
+  MongoURI: dbPassword
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,66 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+//User model
+const User = require("../models/User");
 
 //login page
-router.get("/login", (req, res) => res.send("login"));
-//login page
-router.get("/register", (req, res) => res.send("register"));
+router.get("/login", (req, res) => res.render("login"));
+//register page
+router.get("/register", (req, res) => res.render("register"));
+
+//register handle
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  //check required fields
+  if (!name || !email || !password || password2) {
+    errors.push({ msg: "please fill in all fields" });
+  }
+  //check if passwords match
+  if (password != password2) {
+    errors.push({ msg: "passwords do not match" });
+  }
+
+  //check pass length
+  if (password.length < 6) {
+    errors.push({ msg: "password should be atleast 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    //validation passed
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        //user exists
+        errors.push({ msg: "Email aldready exists" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+        console.log(newUser);
+        res.send("hello");
+      }
+    });
+  }
+});
 
 module.exports = router;
